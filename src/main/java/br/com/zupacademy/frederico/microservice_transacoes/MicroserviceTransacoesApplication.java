ef804c5c4cf9863aa28e82b20617b44dcf67e380
Frederico Neres <neresfrederico@gmail.com>
2021-06-01 18:34:05 -0300

    Correção de inicialização da aplicação.

---
+++ src/main/java/br/com/zupacademy/frederico/microservice_transacoes/MicroserviceTransacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.frederico.microservice_transacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/frederico/microservice_transacoes/MicroserviceTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.frederico.microservice_transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
